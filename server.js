bad3b751cf6d9ff4fb195dac0bbaaf2cf870b1e5
Bader-eng <baderali2016eng@gmail.com>
2021-04-22 15:59:30 +0300

    done

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// make a variable called express to use the express library
+const express = require('express'); //npm install express
+
+// server has all the Express properities and methods
+const server = express();
+
+
+const PORT =  process.env.PORT || 3010;
+
+// the server is ready for listening
+server.listen(PORT, ()=>{
+  console.log(`listening to ${PORT}`);
+});
+
+
+// localhost:3010/./public ->http request (/test->route)
+server.get('/test',(request,response)=>{
+  response.send('Your server is alive!!');
+// console.log(request);
+});
+
+server.get('/',(req,res)=>{
+  res.send('you are in the root');
+});
+
+server.get('/data',(req,res)=>{
+  res.send('you are in the data page');
+});
+
+ server.use(express.static('./public'))
